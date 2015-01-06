a4c2817b1d31df86201c7003b4680030696b828b
demiralp <demiralpali@gmail.com>
2015-01-06 15:09:19 +0200

    Event triggered data binding added.

---
+++ Assets/UDB/Scripts/Unity/EventTriggeredDataBinding.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class EventTriggeredDataBinding : MonoBehaviour 
+{
+    public bool UpdateAtStart = true;
+
+    public ComponentMemberInfo  TriggerEventInfo;
+    public ComponentMemberInfo  SourceInfo;
+    public ComponentMemberInfo  TargetInfo;
+    public DataBindingExpr      DataBindingExpr;
+
+    private EventRef            _triggerEventRef;
+
+    private void Start          ()
+    {
+        DataBindingExpr.Source = SourceInfo.AsDataRef();
+        DataBindingExpr.Target = TargetInfo.AsDataRef();
+
+        _triggerEventRef = TriggerEventInfo.AsEventRef();
+        _triggerEventRef.EventRaised += OnEventRaised;
+
+        if(UpdateAtStart)
+            DataBindingExpr.Update();
+    }
+    private void OnDestroy      ()
+    {
+        if(_triggerEventRef != null)
+            _triggerEventRef.Dispose();
+    }
+    private void OnEventRaised  ()
+    {
+        DataBindingExpr.Update();
+    }
+}
