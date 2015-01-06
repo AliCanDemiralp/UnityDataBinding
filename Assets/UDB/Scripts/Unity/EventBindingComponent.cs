bd7bd95fe3718eb9db717149f79b327cc516e4d1
demiralp <demiralpali@gmail.com>
2015-01-06 15:07:29 +0200

    Event binding wrapper for MonoBehavior added.

---
+++ Assets/UDB/Scripts/Unity/EventBindingComponent.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class EventBindingComponent : MonoBehaviour
+{
+    public bool BindAtStart = true;
+
+    public ComponentMemberInfo  EventInfo;
+    public ComponentMemberInfo  HandlerInfo;
+    public EventBinding         EventBinding;
+
+    public  void Bind()
+    {
+        if (EventInfo == null)
+        {
+            Debug.Log("Event binding unsuccessful. Event info is null!");
+            return;
+        }
+        if (HandlerInfo == null)
+        {
+            Debug.Log("Event binding unsuccessful. Handler info is null!");
+            return;
+        }
+
+        EventBinding.Bind(EventInfo.AsEventRef(), HandlerInfo.AsMethodRef());
+    }
+
+    private void Start      ()
+    {
+        if (BindAtStart)
+            Bind();
+    }
+    private void OnDestroy  ()
+    {
+        if(EventBinding.IsBound)
+            EventBinding.Unbind();
+    }
+
+}
