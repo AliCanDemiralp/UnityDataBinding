ad291a0818ea5609368b8053f70657c744aab962
demiralp <demiralpali@gmail.com>
2015-01-06 15:06:26 +0200

    Reflection extensions for .NET MethodInfo and ParameterInfo added.

---
+++ Assets/UDB/Scripts/Core/MethodInfoExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+public static class MethodInfoExtensions 
+{
+    public static Type[] GetParameterTypes(this MethodInfo methodInfo)
+    {
+        var parameterInfos = methodInfo.GetParameters();
+        var parameterTypes = new Type[parameterInfos.Length];
+        for (var i = 0; i < parameterInfos.Length; i++)
+            parameterTypes[i] = parameterInfos[i].ParameterType;
+        return parameterTypes;
+    }
+
+    public static bool IsParametersCompatible(this MethodInfo methodInfo, ParameterInfo[] parameters)
+    {
+        var parameterInfos = methodInfo.GetParameters();
+        if (parameterInfos.Length != parameters.Length)
+            return false;
+
+        return !parameterInfos.Where((t, i) => !t.IsCompatible(parameters[i])).Any();
+    }
+    public static bool IsParametersCompatible(this MethodInfo methodInfo, MethodInfo other)
+    {
+        return IsParametersCompatible(methodInfo, other.GetParameters());
+    }
+}
---
+++ Assets/UDB/Scripts/Core/ParameterInfoExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+public static class ParameterInfoExtensions
+{
+    public static bool IsCompatible(this ParameterInfo parameterInfo, ParameterInfo other)
+    {
+        return parameterInfo.ParameterType.Equals           (other.ParameterType) ||
+               parameterInfo.ParameterType.IsAssignableFrom (other.ParameterType);
+    }
+}
