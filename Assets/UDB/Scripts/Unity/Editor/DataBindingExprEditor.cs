37c7b2ab3db5c7a371f307bcd2de6d41df4ab7db
demiralp <demiralpali@gmail.com>
2015-02-26 02:48:01 +0100

    Data binding expression editor script added.

---
+++ Assets/UDB/Scripts/Unity/Editor/DataBindingExprEditor.cs
@@ -0,0 +1,32 @@
+﻿using Assets.UDB.Scripts.Core;
+using UnityEngine;
+using UnityEditor;
+
+namespace Assets.UDB.Scripts.Unity
+{
+    [CustomPropertyDrawer(typeof (DataBindingExpr))]
+    public class DataBindingExprEditor : PropertyDrawer
+    {
+        private string _formatString;
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            var contentPosition = EditorGUI.PrefixLabel(position, label);
+            
+            EditorGUI.indentLevel = 0;
+            
+            contentPosition.height = 16;
+
+            EditorGUI.PropertyField(contentPosition, property.FindPropertyRelative("BindingMode"), GUIContent.none);
+
+            // contentPosition.y += 18;
+
+            // EditorGUI.TextArea(contentPosition, _formatString);
+        }
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return 16F;
+            // return 34F;
+        }
+    }
+}
