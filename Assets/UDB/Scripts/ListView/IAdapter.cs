6c84a4135d7219a7be4f7c279cfd6d34d4dd96dc
concubicycle <concubicyclestuffsim@gmail.com>
2015-02-28 18:35:21 -0500

    ListView and IAdapter

---
+++ Assets/UDB/Scripts/ListView/IAdapter.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Assets.UDB.ListView
+{
+    public interface IAdapter
+    {
+        /// <summary>
+        /// Function that determines how a model GameObject should be transformed
+        /// int a Unity UI GameObject (with a ScrollRect). Makes assumptions about which
+        /// Components the model has. 
+        /// </summary>
+        /// <param name="model">A Unity GameObject</param>
+        /// <returns></returns>
+        GameObject ModelToView(GameObject model);
+    }
+}
---
+++ Assets/UDB/Scripts/ListView/ListView.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.UDB.ListView
+{
+    public class ListView : MonoBehaviour
+    {
+        #region Editor
+        public List<GameObject> models;
+        #endregion
+
+
+
+
+        private Dictionary<GameObject, GameObject> _modelToView;
+
+
+        private IAdapter _adapter;
+        public IAdapter Adapter
+        {
+            set
+            {
+                _adapter = value;
+                UpdateList();
+            }
+        }
+
+
+        private ICollection<GameObject> _models;
+        public ICollection<GameObject> Models
+        {
+            set
+            {
+                _models = value;
+                UpdateList();
+            }
+        }
+
+
+        void Start()
+        {
+            _modelToView = new Dictionary<GameObject, GameObject>();
+
+            if (models == null)
+                models = new List<GameObject>();
+
+            UpdateList();
+        }
+
+
+        void Update()
+        {
+
+        }
+
+        #region Private
+        private void UpdateList()
+        {
+            _modelToView.Clear();
+
+
+            //Re-generate model to view.
+            foreach (var go in _models)
+            {
+                //Inflate the view for the model using adapter;
+                GameObject listItem = _adapter.ModelToView(go);
+
+                //TODO: ...
+            }
+        }
+        #endregion
+
+    }
+}
