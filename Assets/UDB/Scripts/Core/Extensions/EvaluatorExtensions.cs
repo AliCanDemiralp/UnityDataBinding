cdd3f9cd09f901403f098e905beeef9746ff9ca3
demiralp <demiralpali@gmail.com>
2015-03-03 19:33:35 +0100

    Added Mono.CSharp.Evaluator extension methods for ease of use.

---
+++ Assets/UDB/Scripts/Core/Extensions/EvaluatorExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Reflection;
+using Mono.CSharp;
+
+namespace Assets.UDB.Scripts.Core.Extensions
+{
+    public static class EvaluatorExtensions
+    {
+        public static void Prepare()
+        {
+            Evaluator.Init(new string[] { });
+            Evaluator.ReferenceAssembly(Assembly.GetExecutingAssembly());
+            foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                var assemblyName = assembly.GetName().Name;
+                if (!(assemblyName.StartsWith("Mono.CSharp") ||
+                      assemblyName.StartsWith("UnityDomainLoad") ||
+                      assemblyName.StartsWith("interactive")))
+                {
+                    try
+                    {
+                        Evaluator.ReferenceAssembly(assembly);
+                    }
+                    catch (Exception)
+                    {
+
+                    }
+                }
+            }
+            Evaluator.Run("using System;");
+            Evaluator.Run("using System.Linq;");
+            Evaluator.Run("using System.Collections;");
+            Evaluator.Run("using System.Collections.Generic;");
+            Evaluator.Run("using UnityEngine;");
+        }
+    }
+}
