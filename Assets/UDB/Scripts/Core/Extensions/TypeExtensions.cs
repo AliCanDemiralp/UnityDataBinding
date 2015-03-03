37a1d6e56f6d3dee5d273121b5988aea9b040843
demiralp <demiralpali@gmail.com>
2015-03-03 19:33:52 +0100

    Added System.Type extensions for ease of use.

---
+++ Assets/UDB/Scripts/Core/Extensions/TypeExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.UDB.Scripts.Core.Extensions
+{
+    public static class TypeExtensions
+    {
+        public static string GetCodeForm(this Type type)
+        {
+            if (!type.IsGenericType) 
+                return type.Name;
+            if (type.IsNested && type.DeclaringType.IsGenericType) 
+                throw new NotImplementedException();
+
+            var txt = type.Name.Substring(0, type.Name.IndexOf('`')) + "<";
+            var cnt = 0;
+            foreach (Type arg in type.GetGenericArguments())
+            {
+                if (cnt > 0) txt += ", ";
+                txt += GetCodeForm(arg);
+                cnt++;
+            }
+            return txt + ">";
+        }
+    }
+}
